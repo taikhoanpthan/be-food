d71719ebf4b836d76a6f3ee92dfa72d25ba7fb37
Anh Mye <taikhoanpthan123@gmail.com>
2024-05-18 23:28:38 +0700

    test

--- models/foodModel.js
+++ models/foodModel.js
@@ -1,13 +1,27 @@
+// import mongoose from "mongoose";
+
+// const foodSchema = new mongoose.Schema({
+//     name: {type:String,required:true},
+//     description: {type:String,required:true},
+//     price: {type:Number,required:true},
+//     image: {type:String,required:true},
+//     category: {type:String,required:true}
+// })
+
+// const foodModel = mongoose.model.food || mongoose.model("food",foodSchema);
+
+// export default foodModel;
 import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema({
-    name: {type:String,required:true},
-    description: {type:String,required:true},
-    price: {type:Number,required:true},
-    image: {type:String,required:true},
-    category: {type:String,required:true}
-})
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  category: { type: String, required: true },
+});
 
-const foodModel = mongoose.model.food || mongoose.model("food",foodSchema);
+// Sửa đổi dòng sau đây: "mongoose.model.food" thành "mongoose.model('Food', foodSchema)"
+const foodModel = mongoose.model("Food", foodSchema);
 
 export default foodModel;
