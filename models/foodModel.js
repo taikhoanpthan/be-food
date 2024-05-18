649cd0b77e3c9f72a2e980b60f9d5476d6443d9e
Anh Mye <taikhoanpthan123@gmail.com>
2024-05-18 23:30:59 +0700

    refix

--- models/foodModel.js
+++ models/foodModel.js
@@ -1,16 +1,3 @@
-// import mongoose from "mongoose";
-
-// const foodSchema = new mongoose.Schema({
-//     name: {type:String,required:true},
-//     description: {type:String,required:true},
-//     price: {type:Number,required:true},
-//     image: {type:String,required:true},
-//     category: {type:String,required:true}
-// })
-
-// const foodModel = mongoose.model.food || mongoose.model("food",foodSchema);
-
-// export default foodModel;
 import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema({
@@ -21,7 +8,6 @@ const foodSchema = new mongoose.Schema({
   category: { type: String, required: true },
 });
 
-// Sửa đổi dòng sau đây: "mongoose.model.food" thành "mongoose.model('Food', foodSchema)"
-const foodModel = mongoose.model("Food", foodSchema);
+const foodModel = mongoose.model.food || mongoose.model("food", foodSchema);
 
 export default foodModel;
--- server.js
+++ server.js
@@ -1,69 +1,30 @@
-// import express from "express";
-// import cors from "cors";
-// import { connectDB } from "./config/db.js";
-// import foodRouter from "./routes/foodRoute.js";
-// import userRouter from "./routes/userRoute.js";
-// import "dotenv/config.js";
-
-// //app config
-// const app = express();
-// const port = 4000;
-
-// //middleware
-// app.use(express.json());
-// app.use(cors());
-
-// // db connection
-// connectDB();
-
-// // api endpoints
-// app.use("/api/food", foodRouter);
-// app.use("/images", express.static("uploads"));
-// app.use("/api/user", userRouter);
-
-// app.get("/", (req, res) => {
-//   res.send("API Working");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server started on http://localhost:${port}`);
-// });
-
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-import dotenv from "dotenv";
-import path from "path";
+import "dotenv/config.js";
 
-// Load environment variables
-dotenv.config();
-
-// App config
+//app config
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
-// Middleware
+//middleware
 app.use(express.json());
 app.use(cors());
 
-// DB connection
+// db connection
 connectDB();
 
-// Serve static files from the "uploads" directory
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// API endpoints
+// api endpoints
 app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 
-// Root endpoint
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
